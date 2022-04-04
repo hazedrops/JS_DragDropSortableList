01947a3552a83c11cf26faf304727b4b7bd71a60
hazedrops <hazedrops@hotmail.com>
2022-04-03 21:26:44 -0700

    Added Swap function and CSS style by checking the answers

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const languages = [
 ]
 
 // Store list items
-const listItems = []
+const listItems = [];
 
-let dragStartIndex
+let dragStartIndex;
 
 createList()
 
@@ -26,10 +26,9 @@ function createList() {
   ;[...languages]
     .map((lang) => ({ value: lang, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
-    .map(a => a.value)
+    .map((a) => a.value)
     .forEach((language, index) => {
       const listItem = document.createElement('li')
-
 
       listItem.setAttribute('data-index', index)
 
@@ -45,4 +44,79 @@ function createList() {
 
       draggable_list.appendChild(listItem)
     })
+
+  addEventListeners()
 }
+
+// Swap list items that are dragged and dropped
+function swapItems(fromIndex, toIndex) {
+  // Getting the items of fromIndex and toIndex
+  const itemOne = listItems[fromIndex].querySelector('.draggable')
+  const itemTwo = listItems[toIndex].querySelector('.draggable')
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+function dragStart() {
+  // console.log('Event: ', 'dragstart');
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+function dragOver(e) {
+  // dragOver event is in the middle of the dragStart and the dragDrop, and prevent the dragDrop function from working, so need to prevent the default action
+  e.preventDefault();
+}
+
+function dragDrop() {
+  // console.log('Event: ', 'drop')
+  const dragEndIndex = +this.getAttribute('data-index');
+
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over')
+
+}
+
+function dragEnter() {
+  // console.log('Event: ', 'dragenter')
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  // console.log('Event: ', 'dragleave')
+  this.classList.remove('over')
+}
+
+// Evaluate the order of list items
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const langName = listItem.querySelector('.draggable').innerText.trim();
+
+    if (langName != languages[index]) {
+      listItem.classList.remove('right');
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable')
+  const draglistItems = document.querySelectorAll('.draggable-list li')
+
+  draggables.forEach((draggable) => {
+    draggable.addEventListener('dragstart', dragStart)
+  })
+
+  draglistItems.forEach((item) => {
+    item.addEventListener('dragover', dragOver)
+    item.addEventListener('drop', dragDrop)
+    item.addEventListener('dragenter', dragEnter)
+    item.addEventListener('dragleave', dragLeave)
+  })
+}
+
+check.addEventListener('click', checkOrder);
